f7026d95d0dac37f2f7a2fef85567f1e8a48195c
making <makingx@gmail.com>
2016-12-13 21:06:50 +0900

    add test

--- src/test/java/mrs/NotificationApplicationTests.java
+++ src/test/java/mrs/NotificationApplicationTests.java
@@ -8,6 +8,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -28,7 +30,6 @@ public class NotificationApplicationTests {
 	}
 
 	@Test
-	@SuppressWarnings("unchecked")
 	public void testFindByUserId() {
 		JsonNode notification1 = restTemplate.postForObject("/v1/notifications",
 				new Notification(NotificationType.ERROR, "error1!", "maki@example.com"),
@@ -68,6 +69,61 @@ public class NotificationApplicationTests {
 		assertThat(ret2.get(0).get("notificationMessage").asText()).isEqualTo("error2!");
 		assertThat(ret2.get(0).get("userId").asText()).isEqualTo("foo@example.com");
 		assertThat(ret2.get(0).get("createdAt").asText()).isNotNull();
+	}
+
+	@Test
+	public void testInvalidRequest() {
+		ResponseEntity<JsonNode> userIdIsEmpty = restTemplate.postForEntity(
+				"/v1/notifications",
+				new Notification(NotificationType.ERROR, "error1!", ""), JsonNode.class);
+		assertThat(userIdIsEmpty.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+		assertThat(userIdIsEmpty.getBody().get("errors").elements()).hasSize(1);
+		assertThat(userIdIsEmpty.getBody().get("errors").get(0).get("entity").asText())
+				.isEqualTo("Notification");
+		assertThat(userIdIsEmpty.getBody().get("errors").get(0).get("property").asText())
+				.isEqualTo("userId");
+		assertThat(
+				userIdIsEmpty.getBody().get("errors").get(0).get("invalidValue").asText())
+						.isEmpty();
+		assertThat(userIdIsEmpty.getBody().get("errors").get(0).get("message").asText())
+				.isEqualTo("may not be empty");
+
+		ResponseEntity<JsonNode> notificationMessageIsEmpty = restTemplate.postForEntity(
+				"/v1/notifications",
+				new Notification(NotificationType.ERROR, "", "maki@example.com"),
+				JsonNode.class);
+		assertThat(notificationMessageIsEmpty.getStatusCode())
+				.isEqualTo(HttpStatus.BAD_REQUEST);
+		assertThat(notificationMessageIsEmpty.getBody().get("errors").elements())
+				.hasSize(1);
+		assertThat(notificationMessageIsEmpty.getBody().get("errors").get(0).get("entity")
+				.asText()).isEqualTo("Notification");
+		assertThat(notificationMessageIsEmpty.getBody().get("errors").get(0)
+				.get("property").asText()).isEqualTo("notificationMessage");
+		assertThat(notificationMessageIsEmpty.getBody().get("errors").get(0)
+				.get("invalidValue").asText()).isEmpty();
+		assertThat(notificationMessageIsEmpty.getBody().get("errors").get(0)
+				.get("message").asText()).isEqualTo("may not be empty");
+
+		ResponseEntity<JsonNode> notificationTypeIsNull = restTemplate.postForEntity(
+				"/v1/notifications",
+				new Notification(null, "error1!", "maki@example.com"), JsonNode.class);
+		assertThat(notificationTypeIsNull.getStatusCode())
+				.isEqualTo(HttpStatus.BAD_REQUEST);
+		assertThat(notificationTypeIsNull.getBody().get("errors").elements()).hasSize(1);
+		assertThat(notificationTypeIsNull.getBody().get("errors").get(0).get("entity")
+				.asText()).isEqualTo("Notification");
+		assertThat(notificationTypeIsNull.getBody().get("errors").get(0).get("property")
+				.asText()).isEqualTo("notificationType");
+		assertThat(notificationTypeIsNull.getBody().get("errors").get(0)
+				.get("invalidValue").asText()).isEqualTo("null");
+		assertThat(notificationTypeIsNull.getBody().get("errors").get(0).get("message")
+				.asText()).isEqualTo("may not be null");
+
+		ResponseEntity<JsonNode> allFieldIsNull = restTemplate.postForEntity(
+				"/v1/notifications", new Notification(null, null, null), JsonNode.class);
+		assertThat(allFieldIsNull.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+		assertThat(allFieldIsNull.getBody().get("errors").elements()).hasSize(3);
 	}
 
 }
