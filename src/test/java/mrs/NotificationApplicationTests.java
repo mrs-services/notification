144ba7e3048688fdc714bb37ee6b3b27dd28a8a9
Toshiaki Maki <makingx@gmail.com>
2016-12-15 03:34:15 +0900

    Make the test robust

--- src/test/java/mrs/NotificationApplicationTests.java
+++ src/test/java/mrs/NotificationApplicationTests.java
@@ -33,25 +33,25 @@ public class NotificationApplicationTests {
 	public final static int UAA_PORT = 39876;
 
 	static SimpleHttpServerFactoryBean factoryBean;
-	static final String ACCESS_TOKEN = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsib2F1dGgyLXJlc291cmNlIl0sInVzZXJfaWQiOiIwMDAwMDAwMC0wMDAwLTAwMDAtMDAwMC0wMDAwMDAwMDAwMDAiLCJ1c2VyX25hbWUiOiJtYWtpQGV4YW1wbGUuY29tIiwic2NvcGUiOlsicmVhZCIsIndyaXRlIl0sImV4cCI6MTQ4MTczMTcyNSwiZ2l2ZW5fbmFtZSI6IlRvc2hpYWtpIiwiZGlzcGxheV9uYW1lIjoiTWFraSBUb3NoaWFraSIsImZhbWlseV9uYW1lIjoiTWFraSIsImF1dGhvcml0aWVzIjpbIlJPTEVfQURNSU4iLCJST0xFX1VTRVIiXSwianRpIjoiNDRmNTBlZTItN2UwMi00N2Q1LTlhMzMtOGNjYmM3YmI3ODEwIiwiY2xpZW50X2lkIjoiMDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDAwMDAwMDAxIn0.NuyICgG14-DkFyesLGxqJH6er6LmmaaSC6s6RAqV8ibzZ5qq1HC3KIGjoTFshBsOKoPqX9eOROmotQdW_AJXc0LisSMWzuHd5E5fOHOrj--5I8FS_KGq2ZazRRLCPcyKddU9hUDPRUM0P5VrX0IYq3AM1hJ_KIXH0zJcMTFm4qyCAICUX41owcew__ZiaoCsrgETwshp1ixwV875dOSkt3PeI77XnCw-7nZY36K8sw2_UM0VYttk_Kd9PGVTs9JPUEnLU5E_mwWDtSahJk-DIuvPUWm-ZpoXXZ_AHR0W0YNQZm28KsY1KuAhqvAhT3Oi5W49VxRHGj1yywpxhbPJJg";
+	static String accessToken;
 
 	@BeforeClass
 	public static void init() throws Exception {
+		TestTokenGenerator tokenGenerator = new TestTokenGenerator();
+		accessToken = tokenGenerator.getAccessToken();
 		factoryBean = new SimpleHttpServerFactoryBean();
 		factoryBean.setPort(UAA_PORT);
-		factoryBean
-				.setContexts(Collections
-						.singletonMap(
-								"/uaa/token_key",
-								(exec) -> {
-									String response = "{\"alg\":\"SHA256withRSA\",\"value\":\"-----BEGIN PUBLIC KEY-----\\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApSmVdhRnVGlYsT3kYkxU\\nR780Amka0qafzl2+qNRWVWcknhVxCZAJ4Y4tQd7D8RpBTbHTQw5jO/gYMKyfLtrM\\nfTv05XXT4I+f2TmspleWOSEQSNvQYfjPjto+/1GkDC0OJTn7xe0oSFuGySu9XJxW\\nemhNZH9beP1N7shw2uX2a6fO6jo/E0S/X0SxIsaZJxyzYoeEc6iovjv1+orC+HZ/\\ngqiT4q0SiRwO72VSu3OmwY95z8J0P4LtpzJfvTok0JreJFv4hauhuxU/qLeDDgga\\nCvYxNRyattFQX+ivXnUcLCt7cwaNvvJSjY6dGLmKhbw7nKwmdpRgKV/s4yhWHS+s\\noQIDAQAB\\n-----END PUBLIC KEY-----\\n\"}";
-									exec.getResponseHeaders().add("Content-Type",
-											"application/json;charset=UTF-8");
-									exec.sendResponseHeaders(200, response.length());
-									try (OutputStream stream = exec.getResponseBody()) {
-										stream.write(response.getBytes());
-									}
-								}));
+		factoryBean.setContexts(Collections.singletonMap(
+				"/uaa/token_key",
+				(exec) -> {
+					String response = tokenGenerator.getTokenKey();
+					exec.getResponseHeaders().add("Content-Type",
+							"application/json;charset=UTF-8");
+					exec.sendResponseHeaders(200, response.length());
+					try (OutputStream stream = exec.getResponseBody()) {
+						stream.write(response.getBytes());
+					}
+				}));
 		factoryBean.afterPropertiesSet();
 	}
 
@@ -68,7 +68,7 @@ public class NotificationApplicationTests {
 	@Test
 	public void testFindByUserId() {
 		HttpHeaders headers = new HttpHeaders();
-		headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + ACCESS_TOKEN);
+		headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
 		JsonNode notification1 = restTemplate.exchange(
 				"/v1/notifications",
 				HttpMethod.POST,
@@ -121,7 +121,7 @@ public class NotificationApplicationTests {
 	@Test
 	public void testInvalidRequest() {
 		HttpHeaders headers = new HttpHeaders();
-		headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + ACCESS_TOKEN);
+		headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
 		ResponseEntity<JsonNode> userIdIsEmpty = restTemplate.exchange(
 				"/v1/notifications", HttpMethod.POST, new HttpEntity<>(new Notification(
 						NotificationType.ERROR, "error1!", ""), headers), JsonNode.class);
