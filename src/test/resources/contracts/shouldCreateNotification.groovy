206d4aab93451d1a39b9199a79f0b844c0206b0f
making <makingx@gmail.com>
2016-12-21 16:21:58 +0900

    Update Contract

--- src/test/resources/contracts/shouldCreateNotification.groovy
+++ src/test/resources/contracts/shouldCreateNotification.groovy
@@ -7,9 +7,9 @@ Contract.make {
         method 'POST'
         url '/v1/notifications'
         body([
-                "notificationType"   : $(consumer(regex('(ERROR|WARN|INFO)')), producer('ERROR')),
+                "notificationType"   : $(consumer(regex('(ERROR|INFO|WARN)')), producer('ERROR')),
                 "notificationMessage": $(consumer(regex('.+')), producer('Your action failed.')),
-                "userId"             : $(consumer(email()), producer('Your action failed.')),
+                "userId"             : $(consumer(regex('.+')), producer('maki@example.com')),
         ])
         headers {
             contentType(applicationJson())
@@ -17,6 +17,8 @@ Contract.make {
     }
     response {
         status 201
+        headers {
+            header 'Location': $(consumer('http://notification/v1/notifications/5d1f9fef-e0dc-4f3d-a7e4-72d2220dd827'), producer(regex('https?://.+/v1/notifications/[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}')))
+        }
     }
-    //createdAt          : value(producer(regex('[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-2][0-9]:[0-5][0-9]:[0-5][0-9].[0-9]{3}+0000'))),
 }
